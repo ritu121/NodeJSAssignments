14cf30b6618bc029dd2f199f4d0e2f4b3804e6c9
Ritu Randive <riturandive121@gmail.com>
2022-02-21 11:06:10 +0530

    MY Assignment 1 Committed

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,14 +1,29 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+   
+    return process.argv[process.argv.length-1]
 }
 
+
 function getNameFromEnv() {
-    // Write your code here
+//     // Write your code here
+    return process.env.Name
 }
+
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline=require('readline');
+    const out=readline.createInterface({
+        input:process.stdin,
+        output:process.stdout
+    })
+    out.question((name)=>{jf
+        out.close();
+    })
+    
 }
+
 
 module.exports = {
     getNameFromCommandLine,
